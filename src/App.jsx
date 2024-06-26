2574d251a80d6d04ff4619690b7cbb230e1b14e0
Berhane Yohannes <berhane.yohannes@capgemini.com>
2024-06-26 13:51:51 +0100

    chore: Refactor App component to use useEffect for count state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,49 @@
-import { useState, useCallback } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
+// eslint-disable-next-line react/prop-types
 function LogoLink({ href, src, alt }) {
   return (
     <a href={href} target="_blank" rel="noreferrer">
       <img src={src} className="logo" alt={alt} />
     </a>
-  )
+  );
 }
 
 function App() {
-  const [count, setCount] = useState(0)
-  const incrementCount = useCallback(() => setCount((count) => count + 1), [])
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    // The code that we want to run
+    console.log("The count is:", count);
+
+    // Optional return function
+    return () => {
+      console.log("I am being cleaned up!");
+    };
+  }, [count]); // The dependency array
 
   return (
     <main>
       <section>
-        <LogoLink href="https://vitejs.dev" src={viteLogo} alt="Vite logo" />
-        <LogoLink href="https://react.dev" src={reactLogo} alt="React logo" className="react" />
+        <LogoLink
+          href="https://react.dev/reference/react/useEffect"
+          src={reactLogo}
+          alt="React logo"
+          className="react"
+        />
       </section>
-      <h1>Yohannes + Laura</h1>
+      <h1>useEffect</h1>
       <section className="card">
-        <button onClick={incrementCount}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <h1>Count: {count}</h1>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
       </section>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;

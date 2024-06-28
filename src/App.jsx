2620ca56f53d84358bd743923a79a00f2c972892
Berhane Yohannes <berhane.yohannes@capgemini.com>
2024-06-28 14:02:45 +0100

    using `useMemo` to memoize the selected item

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,30 @@
-import { useState, useCallback } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMemo, useState } from 'react';
 
-function LogoLink({ href, src, alt }) {
+const initialItems = new Array(29_999_999).fill(0).map((_, i) => {
+  return {
+    id: i,
+    isSelected: i === 29_999_998,
+  };
+});
+
+function Demo() {
+  const [count, setCount] = useState(0); // setter is used
+  const [items] = useState(initialItems); // setter is not used
+
+  const selectedItem = useMemo(
+    () => items.find((item) => item.id === count),
+    [count, items],
+  );
+
   return (
-    <a href={href} target="_blank" rel="noreferrer">
-      <img src={src} className="logo" alt={alt} />
-    </a>
-  )
+    <div className='card'>
+      <h1>Count: {count}</h1>
+      <h1>Selected Item: {selectedItem?.id}</h1>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
 }
 
-function App() {
-  const [count, setCount] = useState(0)
-  const incrementCount = useCallback(() => setCount((count) => count + 1), [])
-
-  return (
-    <main>
-      <section>
-        <LogoLink href="https://vitejs.dev" src={viteLogo} alt="Vite logo" />
-        <LogoLink href="https://react.dev" src={reactLogo} alt="React logo" className="react" />
-      </section>
-      <h1>Yohannes + Laura</h1>
-      <section className="card">
-        <button onClick={incrementCount}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </section>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </main>
-  )
-}
-
-export default App
+export default Demo;
